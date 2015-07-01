3b4adfb5feecccebd9ac917c2717affd492be149
leonardn <leonardnavarez@gmail.com>
2015-07-02 00:59:07 +0800

    Agrimooc yii advance template full source code.

---
+++ advanced/frontend/views/site/index.php
@@ -0,0 +1,160 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'Agrimooc';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Take the world's best courses, online, for free.</h1>
+
+        <p class="lead">Learn from 1,051 courses, from our 122 partners.</p>
+
+        <p><a class="btn btn-lg btn-success" href="#">Get started with Agrimooc</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <h2 class="text-center">Pinaka sikat</h2>
+
+            <div class="col-sm-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-sm-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-sm-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-sm-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <h2 class="text-center">Learner Recommended</h2>
+
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h4>Heading</h4>
+                <img src="http://placehold.it/200x200" alt="..." class="img-rounded">
+                
+                <p>Small heading content...</p>
+
+                <p><a class="btn btn-default" href="#">Heading &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
